ee439b60281cc3ebefb239df6cbf18f12a1d4706
RfrjtntBvzyfljdytcnb <marina-0984@bk.ru>
2023-01-22 00:49:17 +0300

    17.9.1

---
+++ 17.9.1.py
@@ -0,0 +1,35 @@
+array = [int(x) for x in input("Введите числа от 1 до 999 в любом порядке, через пробел: ").split()]
+
+def merge_sort(array):  # "разделяй"
+    if len(array) < 2:  # если кусок массива равен 2,
+        return array[:]  # выход из рекурсии
+    else:
+        middle = len(array) // 2  # ищем середину
+        left = merge_sort(array[:middle])  # рекурсивно делим левую часть
+        right = merge_sort(array[middle:])  # и правую
+        return merge(left, right)  # выполняем слияние
+
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+print(merge_sort(array))
